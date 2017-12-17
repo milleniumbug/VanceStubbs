42e59a15ccfadde9e6fd5a1a59b2939b24e183d0
milleniumbug <milleniumbug@protonmail.com>
2017-12-17 06:20:21 +0100

    Add Paket

---
+++ VanceStubbs.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanceStubbs.Tests
+{
+    public class Class1
+    {
+    }
+}
