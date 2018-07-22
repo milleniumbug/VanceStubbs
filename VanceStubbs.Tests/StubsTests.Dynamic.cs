42cadc9a1aa1cba6d68bbab818b99788395b3a2c
milleniumbug <milleniumbug@protonmail.com>
2018-07-22 13:08:16 -0700

    Test with more types

--- VanceStubbs.Tests/StubsTests.Dynamic.cs
+++ VanceStubbs.Tests/StubsTests.Dynamic.cs
@@ -2,6 +2,8 @@ namespace VanceStubbs.Tests
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
     using NUnit.Framework;
 
     public partial class StubsTests
@@ -9,6 +11,104 @@ namespace VanceStubbs.Tests
         [TestFixture]
         public class Dynamic
         {
+            private IEnumerable<Type> types = Array.AsReadOnly(new Type[]
+            {
+                // void
+                typeof(void),
+
+                // CLS-compliant primitives
+                typeof(bool),
+                typeof(byte),
+                typeof(short),
+                typeof(int),
+                typeof(long),
+                typeof(char),
+                typeof(float),
+                typeof(double),
+
+                // non-CLS compliant primitives
+                typeof(sbyte),
+                typeof(ushort),
+                typeof(uint),
+                typeof(ulong),
+
+                // non-primitives, but built-in to C#
+                typeof(decimal),
+                typeof(string),
+
+                // runtime special types
+                typeof(object),
+                typeof(System.Array),
+                typeof(System.ValueType),
+                typeof(System.Enum),
+                typeof(System.Delegate),
+                typeof(System.MulticastDelegate),
+                typeof(System.Type),
+                typeof(System.TypedReference),
+                typeof(System.IntPtr),
+                typeof(System.UIntPtr),
+                typeof(System.Exception),
+                typeof(System.MarshalByRefObject),
+                typeof(System.ContextBoundObject),
+                typeof(System.ArgIterator),
+                typeof(System.Attribute),
+
+                // custom struct type
+                typeof(System.DateTime),
+
+                // an array
+                typeof(int[]),
+                typeof(DateTime[]),
+                typeof(string[]),
+
+                // an enum
+                typeof(System.IO.FileAccess),
+
+                // an interface
+                typeof(System.IDisposable),
+
+                // an attribute
+                typeof(System.ObsoleteAttribute),
+
+                // an abstract class
+                typeof(System.IO.Stream),
+
+                // an abstract sealed (static) class
+                typeof(System.Linq.Enumerable),
+
+                // open generic class
+                typeof(System.Collections.Generic.List<>),
+
+                // closed generic class
+                typeof(System.Collections.Generic.List<int>),
+
+                // open generic interface
+                typeof(System.Collections.Generic.IList<>),
+
+                // closed generic interface
+                typeof(System.Collections.Generic.IList<int>),
+
+                // open generic delegate
+                typeof(System.Func<,,>),
+
+                // closed generic delegate
+                typeof(System.Func<IList<int>, int, IList<int>>),
+
+                // nullable and friends
+                typeof(Nullable<>),
+                typeof(int?),
+                typeof(double?),
+
+                // generic type parameters
+                typeof(IList<>).GetGenericArguments()[0],
+                typeof(Nullable<>).GetGenericArguments()[0],
+
+                // pointers
+                typeof(char*),
+                typeof(int*),
+                typeof(void*),
+            });
+
             [Test]
             public void TypeLookupBasic()
             {
@@ -61,6 +161,17 @@ namespace VanceStubbs.Tests
                 Assert.AreEqual("list", d[typeof(List<string>)]);
                 Assert.AreEqual("nullable", d[typeof(double?)]);
                 Assert.AreEqual("int", d[typeof(int)]);
+            }
+
+            [Test]
+            public void TypeLookupKillerTest()
+            {
+                var d = new TypeDictionary<Type>(this.types
+                    .Where(t => !t.IsGenericParameter)
+                    .Select((t, i) => new KeyValuePair<Type, Type>(t, t)));
+                Assert.AreEqual(typeof(List<int>), d[typeof(List<int>)]);
+                Assert.AreEqual(typeof(Attribute), d[typeof(DescriptionAttribute)]);
+                Assert.AreEqual(typeof(void), d[typeof(void)]);
             }
         }
     }
